ab089b65116c6c4525c1544318b5e38338bb40cf
Arman Khandaker <arman.khandaker@yahoo.com>
2016-08-23 13:28:50 +0600

    Implement code generator

---
+++ codegen/codegen.c
@@ -0,0 +1,320 @@
+//This is a generator for various repetitive code and strings related to the opcodes.
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+//Holds properties of an instruction 
+struct INSTRUCTION {
+	int opcode;			// the opcode of the instruction in hex
+	char* mnemonic;		// the mnemonic representation. eg. 'add', 'sub'
+	char* operands[2];	// operands of this instruction. non-existent operands are marked null
+	int length;			// the length of this instruction in bytes (not quite needed)
+	int cycles;			// the number of CPU cycles that this instruction takes
+	// 4 bytes representing Z,N,H,C flags
+	//Z - Zero flag, N - Subtract flag, H - Half Carry Flag, C - Carry Flag
+	//If a flag is not affected by this instruction, it is marked by '-'
+	//If a flag is affected by this instruction, it is marked by its corresponding letter, 'Z', 'H', etc
+	//If a flag is set by this instruction, it is marked by 1
+	//If a flag is reset by this instruction, it is marked by 0
+	char flags[4];		
+};
+
+void read_opdb(struct INSTRUCTION* inst[], char* filename);
+void init_inst(struct INSTRUCTION* inst[], int i);
+bool resolve_key_value_pair(struct INSTRUCTION* inst[], int i, char* key, char* value);
+void generate_strings(char* filename, struct INSTRUCTION* inst_op[], struct INSTRUCTION* inst_cbop[]);
+void g_format_string(FILE* fp, char* str);
+char* str_replace(char* str, char* orig, char* replace);
+void print_opdb(struct INSTRUCTION* inst[]);
+
+int main(int argc, char* argv[])
+{
+	struct INSTRUCTION* inst_op[256];	//contains non-prefixed instructions
+	struct INSTRUCTION* inst_cbop[256];	//contains prefixed(CB) instructions
+	
+	if (argc < 3)
+	{
+		//Display command format
+		fprintf(stderr, "Please provide parameters like this: \n> codegen [option] [filename]\n");
+		fprintf(stderr, "Available options: \n");
+		fprintf(stderr, "-s option generates strings for the disassembler");
+		return -1;
+	}
+	char* option = argv[1];
+	char* filename = argv[2];
+	
+	//Load the instructions from db to memory
+	read_opdb(inst_op, "opdb.txt");
+	read_opdb(inst_cbop, "opcbdb.txt");
+	
+	//Generate strings to be used in our disassembler
+	if (!strcmp(option, "-s"))
+	{
+		generate_strings(filename, inst_op, inst_cbop);
+	}
+	
+	return 0;
+}
+
+//Loads the instructions from db to memory
+void read_opdb(struct INSTRUCTION* inst[], char* filename)
+{
+	int szline = 500;	//size of buffer
+	char* line;			//buffer to store a line
+	char* key;			//the key part of key-value pairs
+	char* value;		//the value part of key-value pairs
+	
+	//Open the file specified by filename
+	FILE* fp = fopen(filename, "r");	
+	if (fp == NULL)
+	{
+		perror("Check if opdb.txt and opcbdb.txt exist in current folder");
+		return;
+	}
+	
+	//Allocate memory for buffer
+	line = (char*) malloc(szline+1);
+	int i;	
+	for (i = 0; line && i < 256; i++)
+	{
+		//Allocate memory for current instruction block
+		init_inst(inst, i);		
+		for (;;)
+		{
+			if (fgets(line, szline, fp) == NULL) 
+			{
+				//If reached end-of-file, break
+				free(line);
+				line = NULL;
+				break;
+			}
+			if (!strcmp(line, "\n")) break; 		//If newline reached, move on to next instruction block
+			if (*line == '/' && *(line+1) == '/') 	//skip comments
+			{
+				i--;
+				break;
+			}
+			
+			key = strtok(line, " ");				//extract key
+			value =  strtok(NULL, "\n"); 			//extract value
+			//resolve the key/value pair
+			if (!resolve_key_value_pair(inst, i, key, value))
+			{
+				//false is returned if the instruction was invalid
+				//the next line therefore should be a newline
+				//consume this line to move on to the next instruction block
+				fgets(line, szline, fp);			
+				break;
+			}
+		}
+	}
+	fclose(fp);
+}
+
+//Allocates memory for an instruction block and gives the properties default values
+void init_inst(struct INSTRUCTION* inst[], int i)
+{
+	inst[i] = malloc(sizeof(struct INSTRUCTION));
+	inst[i]->opcode = 0;
+	inst[i]->mnemonic = NULL;
+	inst[i]->operands[0] = NULL;
+	inst[i]->operands[1] = NULL;
+	inst[i]->length = 0;
+	inst[i]->flags[0] = '-';
+	inst[i]->flags[1] = '-';
+	inst[i]->flags[2] = '-';
+	inst[i]->flags[3] = '-';
+}
+
+//Resolves the key/value pair
+bool resolve_key_value_pair(struct INSTRUCTION* inst[], int i, char* key, char* value)
+{
+	int szmnemonic = 10;	//size of a mnemonic
+	int szoperand = 20;		//size of an operand
+	
+	if (!strcmp(key, "opcode:"))
+	{
+		//this was not really needed, as the opcodes are generated linearly 
+		//from 0x00 to 0xFF. Did it just for the heck of it.
+		sscanf(value, "0x%02x", &(inst[i]->opcode));
+	}
+	else if (!strcmp(key, "mnemonic:"))
+	{
+		inst[i]->mnemonic = (char*) malloc(szmnemonic+1);
+		strncpy(inst[i]->mnemonic, value, szmnemonic);
+		//return false if this instruction is invalid
+		if (!strcmp(value, "invalid")) return false; 
+	}
+	else if (!strcmp(key, "operand:") || !strcmp(key, "operand1:"))
+	{
+		inst[i]->operands[0] = (char*) malloc(szoperand+1);
+		strncpy(inst[i]->operands[0], value, szoperand);
+	}
+	else if (!strcmp(key, "operand2:"))
+	{
+		inst[i]->operands[1] = (char*) malloc(szoperand+1);
+		strncpy(inst[i]->operands[1], value, szoperand);
+	}
+	else if (!strcmp(key, "length:"))
+	{
+		inst[i]->length = atoi(value);		//convert the value from string to int
+	}
+	else if (!strcmp(key, "cycles:"))
+	{
+		inst[i]->cycles = atoi(value);
+	}
+	else if (!strcmp(key, "flags:"))
+	{
+		int j;
+		for (j = 0; j < 4; j++) 
+		{
+			inst[i]->flags[j] = value[j];	//fetch the flags
+		}
+	}
+	
+	return true;
+}
+
+//Prints a mirror of opdb or opcbdb (without comments)
+//Was created initially for testing purposes. Not needed anymore
+void print_opdb(struct INSTRUCTION* inst[])
+{
+	int i;
+	for (i = 0; i < 256; i++)
+	{
+		printf("opcode: 0x%02X\n", inst[i]->opcode);
+		printf("mnemonic: %s\n", inst[i]->mnemonic);
+		if (!strcmp(inst[i]->mnemonic, "invalid"))
+		{
+			printf("\n");
+			continue;
+		}
+		
+		if (inst[i]->operands[0] != NULL && inst[i]->operands[1] != NULL)
+		{
+			printf("operand1: %s\n", inst[i]->operands[0]);
+			printf("operand2: %s\n", inst[i]->operands[1]);
+		}
+		
+		if (inst[i]->operands[0] != NULL && inst[i]->operands[1] == NULL)
+		{
+			printf("operand: %s\n", inst[i]->operands[0]);
+		}
+		
+		printf("length: %d\n", inst[i]->length);
+		printf("cycles: %d\n", inst[i]->cycles);
+		char* f = inst[i]->flags;
+		printf("flags: %c%c%c%c\n", f[0], f[1], f[2], f[3]);
+		printf("\n");
+	}
+}
+
+//Generates strings for disassembler
+void generate_strings(char* filename, struct INSTRUCTION* inst_op[], struct INSTRUCTION* inst_cbop[])
+{
+	int i;
+	char* tmp;
+	FILE* fp = fopen(filename, "w");	
+	if (fp == NULL)
+	{
+		perror("Failed to generate file");
+		return;
+	}
+	
+	fprintf(fp, "//This file is auto-generated by codegen. Don't modify it directly\n");
+	fprintf(fp, "//Either modify the opcode db or the generation logic in codegen\n");
+	fprintf(fp, "//These are strings needed by the disassembler (produced by -s)\n\n");
+	
+	//Generate strings for non-prefixed instructions
+	for (i = 0; i < 256; i++)
+	{
+		if (inst_op[i]->opcode != 0xCB)		//Skip the prefix instruction
+		{
+			fprintf(fp, "\"%-5s  ", inst_op[i]->mnemonic);
+			//format the operands with a format string in case the operand is a 8/16-bit data/address
+			if (inst_op[i]->operands[0] != NULL)
+				g_format_string(fp, inst_op[i]->operands[0]);				
+			if (inst_op[i]->operands[1] != NULL)
+			{
+				fprintf(fp, ", ");
+				g_format_string(fp, inst_op[i]->operands[1]);
+			}
+			fprintf(fp, "\", 13, 10, 0\n");
+		}
+	}
+	fprintf(fp, "\n");
+	//Generate strings for prefixed(CB) instructions
+	for (i = 0; i < 256; i++)
+	{
+		fprintf(fp, "\"%-5s  ", inst_cbop[i]->mnemonic);
+		if (inst_cbop[i]->operands[0] != NULL) 
+			g_format_string(fp, inst_cbop[i]->operands[0]);				
+		if (inst_cbop[i]->operands[1] != NULL)
+		{
+			fprintf(fp, ", ");
+			g_format_string(fp, inst_cbop[i]->operands[1]);
+		}
+		fprintf(fp, "\", 13, 10, 0\n");
+	}
+	
+	fclose(fp);
+}
+
+//Formats the operands with a format string in case the operand is a 8/16-bit data/address
+void g_format_string(FILE* fp, char* str)
+{
+	if (strstr(str, "d8"))
+	{
+		fprintf(fp, "%s", str_replace(str, "d8", "0x%02x"));
+	}
+	else if (strstr(str, "a8"))
+	{
+		fprintf(fp, "%s", str_replace(str, "a8", "0x%02x"));
+	}
+	else if (strstr(str, "r8"))
+	{
+		fprintf(fp, "%s", str_replace(str, "r8", "0x%02x"));
+	}
+	else if (strstr(str, "d16"))
+	{
+		fprintf(fp, "%s", str_replace(str, "d16", "0x%02x%02x"));
+	}
+	else if (strstr(str, "a16"))
+	{
+		fprintf(fp, "%s", str_replace(str, "a16", "0x%02x%02x"));
+	}
+	else
+	{
+		//in case there are no r8, a8, d8, a16, d16 tokens in the operand,
+		//print the string without formatting
+		fprintf(fp, "%s", str);
+	}
+}
+
+//A helper function which replaces orig from str with rep
+//Not really important to understand
+char* str_replace(char* str, char* orig, char* rep)
+{
+	int diff = strlen(rep) - strlen(orig);
+	char* newstr = (char*) malloc(strlen(str)+diff+1);
+	char* tmp = newstr;
+	char* off = strstr(str, orig);
+	while (str != off) 
+	{
+		*(tmp++) = *(str++); 
+	}
+	while (*rep != 0)
+	{
+		*(tmp++) = *(rep++);
+	}
+	off += strlen(orig);
+	while (*off != 0)
+	{
+		*(tmp++) = *(off++);
+	}
+	*tmp = 0;
+	
+	return newstr;
+}
---
+++ disassembler/disasm.asm
@@ -0,0 +1,384 @@
+; Personal Project
+; A Nintendo Gameboy Emulator
+;
+; =======================================================================================================
+; Disassembler for Gameboy (LR35902) ROMs
+; =======================================================================================================
+; Starting in: 18/9/16
+; More description to be added here 
+;
+
+format PE console
+entry start
+
+include 'win32a.inc'						; contains common structs and constants for WINAPI
+include 'encoding/utf8.inc'					; contains support for UTF-8
+include 'disasm_h.inc'						; contains some constants from WINAPI not present in win32a
+
+; =======================================================================================================
+; Some constants
+;
+size_KB = 1024						
+size_MB = size_KB * size_KB					; stands for 1 MB
+
+; =======================================================================================================
+section '.data' data readable writable
+	;printf_s	du "%s",13,10,0				; unicode format string for a string
+	printf_d	db "%d",13,10,0				; format string for a decimal number
+
+; =======================================================================================================
+;section '.bss'  readable writable
+;	wow1	db ?
+
+; =======================================================================================================
+section '.text' code readable executable
+
+; =======================================================================================================
+; start
+; command-line arguments=>
+; filename of the rom
+; operation=>
+; reads the file specified into memory
+;
+
+start:
+	.pNumArgs = -4h							; number of arguments from command line
+	.szArglist = -8h						; address of the list of addresses to arguments
+	.mallocRom = -0ch						; address of the dynamically allocated memory to store ROM data
+	.inFilename = -10h						; address of the name of the input file
+	.bytesRead = -14h						; number of bytes actually read from the file
+	.outFilename = -18h						; address of the name of the output file
+	.bytesWritten = -1ch					; number of bytes actually written to the new file
+	sub	esp, 4*7							; keep space for local variables
+
+	call	print_intro						; print an intro to the disassembler
+
+	call	[GetCommandLineW]				; fetch the command line string
+	; call CommandLineToArgvW to transform the command line string into an array of arguments
+	lea     ebx, [ebp+.pNumArgs]
+	push	ebx					
+	push	eax
+	call	[CommandLineToArgvW]
+	mov 	dword[ebp+.szArglist], eax
+	cmp	dword[ebp+.pNumArgs], 1
+	jle	.commandLinetoArgvW_fail			; raise error if there are less than 2 arguments
+	
+	mov	ebx, dword[eax+4]					; save the address of the first argument
+	mov	dword[ebp+.inFilename], ebx		
+	;mov	ebx, dword[eax+8]				; save the address of the second argument
+	;mov	dword[ebp+.outFilename], ebx		
+
+	; print the unicode filename
+	;push    ebx
+	;push	printf_s
+	;call	[wprintf]
+	;add	esp, 8
+	;jmp	.exit
+
+	; Gameboy ROMS are usually 1024 MB in size. 
+	; An extra byte is being allocated for the terminating null byte.
+	push	size_MB+1				
+	call	[malloc]
+	add		sp, 4							; _cdecl style
+	cmp		eax, 0
+	je		.malloc_fail					; raise error if null was returned
+	mov		dword[ebp+.mallocRom], eax
+
+
+	; Read the rom from file to memory
+	push	dword[ebp+.inFilename]
+	push	size_MB
+	push	dword[ebp+.mallocRom]
+	call	read_rom
+	add		esp, 4*3
+	cmp		eax, 0							; exit if file couldn't be read (error is already raised in read_rom)
+	je		.io_rom_fail
+	mov		dword[ebp+.bytesRead], eax
+	
+	
+
+	;push	dword[ebp+.bytesRead]
+	;push	printf_d
+	;call	[printf]						; print the number of bytes read
+	;add	esp, 4*2
+
+
+	; Write the rom from memory to file
+	;push	dword[ebp+.outFilename]
+	;push	dword[ebp+.bytesRead]
+	;push	dword[ebp+.mallocRom]
+	;call	write_rom
+	;add	esp, 4*3
+	;cmp	eax, 0							; exit if file couldn't be written (error is already raised in write_rom)
+	;je	.io_rom_fail
+	;mov	dword[ebp+.bytesWritten], eax
+
+	;push	dword[ebp+.bytesWritten]
+	;push	printf_d
+	;call	[printf]						; print the number of bytes written
+	;add	esp, 4*2
+
+.io_rom_fail:
+	push	dword[ebp+.mallocRom]
+	call	[free]							; free the allocated memory
+	add		esp, 4
+	jmp		.exit
+
+.commandLinetoArgvW_fail:
+	jmp		.skip_commandLinetoArgvW_fail_data
+	.commandLinetoArgvW_fail_data	db 'Please provide a filename as an argument like this:', 13, 10
+									db '>disasm.exe inxxxRom.gb', 13, 10, 0
+.skip_commandLinetoArgvW_fail_data:
+	push	.commandLinetoArgvW_fail_data
+	call	[printf]						; here if no argument was provided
+	add		esp, 4
+	jmp		.exit
+
+.malloc_fail:
+	jmp		.skip_malloc_fail_data
+	.malloc_fail_data	db 'Failed to allocate space for loading ROM.', 13, 10, 0
+.skip_malloc_fail_data:
+	push	.malloc_fail_data
+	call	[printf]						; here if malloc failed
+	add		esp, 4
+	jmp		.exit
+
+.exit:
+	push	dword[ebp+.szArglist]		
+	call	[LocalFree]						; free memory used by CommandLineToArgvW		
+
+	add		esp, 4*7
+	
+	push	0
+	call 	[ExitProcess]					; Exit the process
+
+; =======================================================================================================
+; int read_rom (char* buffer, int size, char* filename)
+; parameters=>
+; buffer: buffer to load the rom into
+; size: maximum number of bytes to read
+; filename: name of the rom
+; returns=>
+; number of bytes actually read
+; operation=>
+; Reads the rom from file to a buffer of given maximum size
+;
+read_rom:
+	.buffer = 8h					
+	.size = 0ch
+	.filename_addr = 10h
+	.lpNumberOfBytesRead = -4h				; number of bytes actually read
+	.fileHandle = -8h						; a handle to the opened file (the ROM)
+	push	ebp
+	mov 	ebp, esp						; create stack frame
+	sub		esp, 4*2
+
+	push	ebx ecx edx esi edi				; keep backup of registers
+
+	; call CreateFileW to open a handle to the ROM file
+	push	0
+	push	FILE_ATTRIBUTE_NORMAL
+	push	OPEN_EXISTING
+	push	0
+	push 	FILE_SHARE_READ
+	push	GENERIC_READ
+	push	dword[ebp+.filename_addr]
+	call	[CreateFileW]
+	cmp		eax, INVALID_HANDLE_VALUE
+	je		.error							; raise error if the handle is invalid
+	mov		dword[ebp+.fileHandle], eax
+
+	; call ReadFile to read the ROM handle to memory
+	push	0
+	lea		ebx, [ebp+.lpNumberOfBytesRead]
+	push	ebx 
+	push	dword[ebp+.size]
+	push	dword[ebp+.buffer]
+	push	dword[ebp+.fileHandle]
+	call	[ReadFile]
+	cmp		eax, 0
+	je		.error							; raise error if null is returned
+
+	; call CloseHandle to close the handle to the ROM file (to release resources)
+	push	dword[ebp+.fileHandle]
+	call	[CloseHandle]
+	
+	mov		eax, dword[ebp+.lpNumberOfBytesRead]	; return number of bytes read
+	jmp		.exit
+
+.error:
+	; call GetLastError to check what the last error was
+	call	[GetLastError]
+	cmp		eax, ERROR_FILE_NOT_FOUND
+	je		.file_not_found				
+	jmp		.skip_unknown_error_data		
+	.unknown_error_data		db 'Reading file - unknown error', 13, 10, 0
+.skip_unknown_error_data:
+	push	.unknown_error_data
+	call	[printf]						; here if the error is unknown
+	add		esp, 4
+	mov		eax, 0							; return null
+	jmp		.exit
+
+.file_not_found:
+	jmp		.skip_file_not_found_data
+	.file_not_found_data	db 'The system cannot find the file specified.', 13, 10, 0
+.skip_file_not_found_data:
+	push	.file_not_found_data
+	call	[printf]						; here if the file was not found
+	add		esp, 4
+	mov 	eax, 0
+	jmp		.exit
+
+.exit:
+	pop	edi esi edx ecx ebx					; restore backed up registers
+
+	add		esp, 4*2
+	pop		ebp								; destroy stack frame
+	ret
+
+; =======================================================================================================
+; int write_rom (char* buffer, int size, char* filename)
+; parameters=>
+; buffer: buffer containing the ROM to write
+; size: maximum number of bytes to write
+; filename: name of the new file (ROM)
+; returns=>
+; number of bytes actually written
+; operation=>
+; Writes the rom from memory to a file of given maximum size
+;
+write_rom:
+	.buffer = 8h					
+	.size = 0ch
+	.filename_addr = 10h
+	.lpNumberOfBytesWritten = -4h			; number of bytes actually written
+	.fileHandle = -8h						; a handle to the created file (the new ROM)
+	push	ebp
+	mov 	ebp, esp				
+	sub		esp, 4*2
+
+	push	ebx ecx edx esi edi			
+
+	; call CreateFileW to open a handle to a new ROM file
+	push	0
+	push	FILE_ATTRIBUTE_NORMAL
+	push	CREATE_NEW
+	push	0
+	push 	FILE_SHARE_READ
+	push	GENERIC_WRITE
+	push	dword[ebp+.filename_addr]
+	call	[CreateFileW]
+	cmp		eax, INVALID_HANDLE_VALUE
+	je		.error							; raise error if the handle is invalid
+	mov		dword[ebp+.fileHandle], eax
+
+	; call WriteFile to write from memory to ROM handle
+	push	0
+	lea		ebx, [ebp+.lpNumberOfBytesWritten]
+	push	ebx 
+	push	dword[ebp+.size]
+	push	dword[ebp+.buffer]
+	push	dword[ebp+.fileHandle]
+	call	[WriteFile]
+	cmp		eax, 0
+	je		.error							; raise error if null is returned
+
+	; call CloseHandle to close the handle to the new ROM file (to release resources)
+	push	dword[ebp+.fileHandle]
+	call	[CloseHandle]
+	
+	mov		eax, dword[ebp+.lpNumberOfBytesWritten]	; return number of bytes written
+	jmp		.exit
+
+.error:
+	; call GetLastError to check what the last error was
+	call	[GetLastError]
+	cmp		eax, ERROR_FILE_EXISTS
+	je		.file_exists			
+	jmp		.skip_unknown_error_data		
+	.unknown_error_data		db 'Writing file - unknown error', 13, 10, 0
+.skip_unknown_error_data:
+	push	.unknown_error_data
+	call	[printf]						; here if the error is unknown
+	add		esp, 4
+	mov		eax, 0							; return null
+	jmp		.exit
+
+.file_exists:
+	jmp		.skip_file_exists_data
+	.file_exists_data	db 'The file to be written already exists.', 13, 10, 0
+.skip_file_exists_data:
+	push	.file_exists_data
+	call	[printf]						; here if the file already exists
+	add		esp, 4
+	mov 	eax, 0
+	jmp		.exit
+
+.exit:
+	pop	edi esi edx ecx ebx			
+
+	add		esp, 4*2
+	pop		ebp					
+	ret
+
+; =======================================================================================================
+; int decode (void)
+; parameters=>
+; void
+; returns=>
+; void
+; operation=>
+; Prints an intro of the disassembler to the console
+;
+
+
+; =======================================================================================================
+; void print_intro (void)
+; parameters=>
+; void
+; returns=>
+; void
+; operation=>
+; Prints an intro of the disassembler to the console
+;
+print_intro:
+	pushad									; backup all registers
+
+	jmp     .skip_data						; skip over the string
+	.print_intro_data	db 'Welcome to the LR35902 Disassembler!', 13, 10, 0
+.skip_data:
+	push	.print_intro_data
+	call	[printf]						; print the intro
+	add 	esp, 4
+
+	popad									; restore all registers					
+	ret
+
+; =======================================================================================================
+section '.idata' import data readable
+
+library	kernel, 'kernel32.dll', \
+	shell, 'shell32.dll', \
+	msvcrt, 'msvcrt.dll'
+
+import	kernel, \
+	ExitProcess, 'ExitProcess', \
+	CreateFileW, 'CreateFileW', \
+	CloseHandle, 'CloseHandle', \
+	ReadFile, 'ReadFile', \
+	WriteFile, 'WriteFile', \
+	GetCommandLineW, 'GetCommandLineW', \
+	LocalFree, 'LocalFree', \
+	GetLastError, 'GetLastError'
+
+import  shell, \
+	CommandLineToArgvW, 'CommandLineToArgvW'
+
+; _cdecl libraries
+; must balance the stack after calling these subroutines
+import	msvcrt, \
+	printf, 'printf', \
+	wprintf, 'wprintf', \
+	malloc, 'malloc', \
+	free, 'free'
